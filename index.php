4fbce908f7071c8a97d2cd61f2526aa357e559dd
changwoo <ep6tri@hotmail.com>
2020-06-07 15:16:22 +0900

    목업 페이지 제작

--- index.php
+++ index.php
@@ -1,17 +1,14 @@
-<ul>
-	<li>
-		<a href="<?php echo esc_url( get_stylesheet_directory_uri() . '/mockups/issues-list.html' ); ?>">
-			Issues List
-		</a>
-	</li>
-	<li>
-		<a href="<?php echo esc_url( get_stylesheet_directory_uri() . '/mockups/issue-single.html' ); ?>">
-			Issue Single
-		</a>
-	</li>
-	<li>
-		<a href="<?php echo esc_url( get_stylesheet_directory_uri() . '/mockups/issue-edit.html' ); ?>">
-			Issue Edit
-		</a>
-	</li>
-</ul>
+<!DOCTYPE html>
+<html lang="ko">
+<head>
+    <meta charset="UTF-8">
+    <title>백석 이슈 트래커 싱글</title>
+    <link rel="stylesheet"
+          href="<?php echo esc_url( get_stylesheet_directory_uri() . '/assets/css/normalize.css' ); ?>">
+</head>
+<body>
+<a href="<?php echo esc_url( get_stylesheet_directory_uri() . '/mockups/index.html' ); ?>">
+    목업 페이지
+</a>
+</body>
+</html>
