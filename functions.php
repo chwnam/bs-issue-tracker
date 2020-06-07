fbf8267e54e8c491c8875640d451ddd1cc28113d
changwoo <ep6tri@hotmail.com>
2020-06-07 14:27:08 +0900

    functions.php 코드 업데이트

--- functions.php
+++ functions.php
@@ -7,3 +7,16 @@ function bs_register_post_type() {
 }
 
 add_action( 'init', 'bs_register_post_type' );
+
+
+function bs_enqueue_scripts() {
+	wp_register_script(
+		'bs-normalize',
+		get_stylesheet_directory_uri() . '/assets/css/normalize.css',
+		[],
+		'1.0.0',
+		false
+	);
+}
+
+add_action( 'wp_enqueue_scripts', 'bs_enqueue_scripts', 1 );
