89fa37ef4df7e35409dd1e747d6a50db3295cc68
changwoo <ep6tri@hotmail.com>
2020-06-07 15:09:09 +0900

    목업 인덱스 코드 업데이트

---
+++ index.php
@@ -0,0 +1,17 @@
+<ul>
+	<li>
+		<a href="<?php echo esc_url( get_stylesheet_directory_uri() . '/mockups/issues-list.html' ); ?>">
+			Issues List
+		</a>
+	</li>
+	<li>
+		<a href="<?php echo esc_url( get_stylesheet_directory_uri() . '/mockups/issue-single.html' ); ?>">
+			Issue Single
+		</a>
+	</li>
+	<li>
+		<a href="<?php echo esc_url( get_stylesheet_directory_uri() . '/mockups/issue-edit.html' ); ?>">
+			Issue Edit
+		</a>
+	</li>
+</ul>
