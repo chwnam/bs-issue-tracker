dbff6c02de5b9f7c668835163473fd3e399e9514
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-06-07 19:28:04 +0900

    IssueListModel 더미 추가

--- src/App.vue
+++ src/App.vue
@@ -6,6 +6,8 @@
 </template>
 
 <script>
+//Models
+import IssueListModel from './Models/IssueListModel';
 // Pages
 import IssueList from './pages/IssueList'
 import IssueDetail from './pages/IssueDetail'
@@ -19,7 +21,30 @@ export default {
   data() {
     return {
       viewDetail : false
+      issueList: [],
+      openIssueCount: null,
+      closeIssueCount: null
     }
   }
+  methods: {
+    IssueListFetch() {
+      IssueListModel.list().then(data => {
+        this.issueList = data
+      })
+    },
+    IssueCountFetch() {
+      IssueListModel.list().then(data => {
+        let open = 0;
+        let close = 0;
+
+        data.forEach(el => {
+          el.status === 'open' ? open++ : close++
+        });
+
+        this.openIssueCount = open
+        this.closeIssueCount = close
+      })
+    },
+  },
 }
 </script>
