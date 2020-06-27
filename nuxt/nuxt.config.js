92a5deb174f531997aae2b45670e339c4aeeb4fd
kjkandrea <32591477+kjkandrea@users.noreply.github.com>
2020-06-27 19:58:04 +0900

    Nuxt 프레임워크로 프론트엔드 개발분 리펙터링

--- nuxt/nuxt.config.js
+++ nuxt/nuxt.config.js
@@ -60,7 +60,9 @@ export default {
   ** Axios module configuration
   ** See https://axios.nuxtjs.org/options
   */
-  axios: {},
+  axios: {
+    baseURL: process.env.BASE_URL || 'http://127.0.0.1:8080/bs-wordpress/wp-json/wp'
+  },
   /*
   ** vuetify module configuration
   ** https://github.com/nuxt-community/vuetify-module
@@ -68,18 +70,7 @@ export default {
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
-      themes: {
-        dark: {
-          primary: colors.blue.darken2,
-          accent: colors.grey.darken3,
-          secondary: colors.amber.darken3,
-          info: colors.teal.lighten1,
-          warning: colors.amber.base,
-          error: colors.deepOrange.accent4,
-          success: colors.green.accent3
-        }
-      }
+      light: true     
     }
   },
   /*
